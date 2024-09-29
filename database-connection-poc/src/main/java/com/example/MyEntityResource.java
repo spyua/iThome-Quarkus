1bec3218a629f93a3b42812537ab51a4fb558bab
pal2097 <pal2097@gmail.com>
2024-09-29 17:06:51 +0800

    feat:upload poc project

---
+++ database-connection-poc/src/main/java/com/example/MyEntityResource.java
@@ -0,0 +1,6 @@
+package com.example;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
